650790032258ccbe4e8a7b64c06756555fd3af1c
AureKaire <54981480+AureKaire@users.noreply.github.com>
2019-10-10 09:37:08 +0300

    Update CategoryDaoImpl.java

--- src/main/java/com/texteditor/lib/dao/CategoryDaoImpl.java
+++ src/main/java/com/texteditor/lib/dao/CategoryDaoImpl.java
@@ -19,25 +19,13 @@ public class CategoryDaoImpl implements CategoryDao {
     @Override
     @SuppressWarnings("unchecked")
     public List<Category> findAll() {
-        // Open a session
+       
         Session session = sessionFactory.openSession();
-
-        // DEPRECATED as of Hibernate 5.2.0
-        // List<Category> categories = session.createCriteria(Category.class).list();
-
-        // Create CriteriaBuilder
         CriteriaBuilder builder = session.getCriteriaBuilder();
-
-        // Create CriteriaQuery
         CriteriaQuery<Category> criteria = builder.createQuery(Category.class);
 
-        // Specify criteria root
         criteria.from(Category.class);
-
-        // Execute query
         List<Category> categories = session.createQuery(criteria).getResultList();
-
-        // Close session
         session.close();
 
         return categories;
