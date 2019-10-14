38d62255ed60b1d14f8ad82d01cad20a464496de
AureKaire <54981480+AureKaire@users.noreply.github.com>
2019-10-14 18:14:36 +0300

    Update app.js

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -22,32 +22,3 @@ $(function(){
         $(obj.selPlaceholder).attr('style',style);
     };
 
-    var onLoad = function(obj) {
-
-        var $option = $(obj.el).find('option[selected]');
-
-        if($option.length > 0) {
-            // Get option style
-            var style = $(obj.el).find('option[selected]').attr('style');
-
-
-            $(obj.selPlaceholder).attr('style',style);
-
-            // Mark selected item as selected
-            $(obj.selEl).find('ul li[data-value="' + obj.el.value + '"]').addClass('cs-selected');
-        } else {
-            // Unmark list items as cs-selected, since first is selected by default
-            $(obj.selEl).find('ul li').removeClass('cs-selected');
-
-            // Set select element's value to empty string
-            $(obj.el).val('');
-        }
-    };
-
-    // Activate Codrops select magic
-    (function() {
-        [].slice.call( document.querySelectorAll( 'select' ) ).forEach( function(el) {
-            new SelectFx(el,{onLoad:onLoad,onChange:onChange});
-        });
-    })();
-});
