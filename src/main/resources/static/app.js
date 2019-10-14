0cea50ac42f3e986dedcadbabfc56d2eef5bb6f3
AureKaire <54981480+AureKaire@users.noreply.github.com>
2019-10-14 18:24:50 +0300

    Update app.js

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -2,24 +2,5 @@ $(function(){
 
     $(".button-collapse").sideNav({edge: 'right'});
 
-
-    $('[data-close]').on('click',function(){
-        $(this).parent().remove();
-    });
-
-
-    $('input[type=file]').on("change",function(){
-        var value = $(this).val().split('\\').pop();
-        value = value == null || value == '' ? $(this).find('.placeholder').data('placeholder') : value;
-        $(this).parent('.file-wrapper').find('.placeholder').text(value);
-    });
-
-    var onChange = function(obj) {
-
-        var style = $(obj.selEl).find('li.cs-selected').attr('style');
-
-
-        $(obj.selPlaceholder).attr('style',style);
-    };
-    });
+});
 
